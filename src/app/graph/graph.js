3dd6e1c1308670d85e93c14c1f9da751fe39451b
Shirley Jun Yi Ren <sjr255@cornell.edu>
2021-03-16 21:08:45 -0400

    import react; try VictoryChart in graph.html

---
+++ src/app/graph/graph.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import {VictoryChart, VictoryLine} from 'victory';
+
+console.log("graph.js");
+// const data=[
+//     { x: 1, y: 2 },
+//     { x: 2, y: 3 },
+//     { x: 3, y: 5 },
+//     { x: 4, y: 4 },
+//     { x: 5, y: 7 }
+// ];
+
+{/* <VictoryChart>
+  <VictoryLine style={{
+    data: { stroke: "#c43a31" },
+    parent: { border: "1px solid #ccc"}
+  }}
+  data={[
+      { x: 1, y: 2 },
+      { x: 2, y: 3 },
+      { x: 3, y: 5 },
+      { x: 4, y: 4 },
+      { x: 5, y: 7 }
+  ]}
+  />
+</VictoryChart>
+
+function App(){
+    return(
+        <div>
+
+        </div>
+    );
+}; */}
